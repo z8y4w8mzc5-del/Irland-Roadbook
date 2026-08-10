f971153d15b03fd077e7923440db273a46978459
z8y4w8mzc5-del <z8y4w8mzc5@privaterelay.appleid.com>
2026-08-10 16:00:00 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const C='irland-roadbook-v2';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='irland-roadbook-v4';
+const A=['./','./index.html?v=4','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html?v=4')||caches.match('./')));return;}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
